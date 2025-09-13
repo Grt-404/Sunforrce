a8eb14c1e3f88432db9acce0f21df039224c1b63
Gurpreet-2024 <gurpreetarora1406@gmail.com>
2025-09-13 16:12:45 +0530

    Frontend changes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (user) => {
     return jwt.sign(
         { email: user.email, id: user._id, role: user.role },
         process.env.JWT_KEY,
-        { expiresIn: '7d' } // optional: token expires in 7 days
+        { expiresIn: '7d' }
     );
 };
 
