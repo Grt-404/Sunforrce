67220b3b65b5ce14be27a370e7b6d58ac93eb7c3
Aa5hut0sh <aashutoshsharma2905@gmail.com>
2025-09-29 11:44:23 +0530

    Authentication error solve

--- middlewares/isLoggedin.js
+++ middlewares/isLoggedin.js
@@ -4,37 +4,37 @@ const studentModel = require("../models/student-model");
 const collegeModel = require("../models/college-model");
 
 module.exports = async function (req, res, next) {
-    if (!req.cookies.token) {
-        req.flash("error", "You need to login first");
-        return res.redirect("/explore");
+  if (!req.cookies.token) {
+    req.flash("error", "You need to login first");
+    return res.redirect("/");
+  }
+
+  try {
+    const decoded = jwt.verify(req.cookies.token, process.env.JWT_KEY);
+
+    let Model;
+    if (decoded.role === "alumni") Model = alumniModel;
+    else if (decoded.role === "student") Model = studentModel;
+    else if (decoded.role === "college") Model = collegeModel;
+    else {
+      req.flash("error", "Invalid user role");
+      return res.redirect("/");
     }
 
-    try {
+    const user = await Model.findOne({ email: decoded.email }).select(
+      "-password"
+    );
 
-        const decoded = jwt.verify(req.cookies.token, process.env.JWT_KEY);
-
-        let Model;
-        if (decoded.role === "alumni") Model = alumniModel;
-        else if (decoded.role === "student") Model = studentModel;
-        else if (decoded.role === "college") Model = collegeModel;
-        else {
-            req.flash("error", "Invalid user role");
-            return res.redirect("/explore");
-        }
-
-
-        const user = await Model.findOne({ email: decoded.email }).select("-password");
-
-        if (!user) {
-            req.flash("error", "User not found");
-            return res.redirect("/explore");
-        }
-
-        req.user = user;
-        next();
-    } catch (err) {
-        console.error(err.message);
-        req.flash("error", "Something went wrong");
-        res.redirect("/explore");
+    if (!user) {
+      req.flash("error", "User not found");
+      return res.redirect("/");
     }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error(err.message);
+    req.flash("error", "Something went wrong");
+    res.redirect("/");
+  }
 };
